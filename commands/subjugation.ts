466e719d8223d8210d556914e2e38184f4543f4a
nayukata <natume31612@gmail.com>
2021-07-13 07:45:38 +0900

    feat: notification message

--- commands/subjugation.ts
+++ commands/subjugation.ts
@@ -7,46 +7,46 @@ import { setRemainder } from '../libs/schedule'
 
 module.exports = {
   name: 'subjugation',
-  aliases: ['kill', 'done'],
+  aliases: ['kill', 'done', 'kl'],
   description:
     'ネームドモンスターの討伐時間を記録します。\n`/[command name] モンスター名` で討伐時間の記録ができます。',
   execute(message: Message, args: string[]) {
     const monsterName = args[0]
 
     let resMessage = 'そんなモンスターいないわよ！'
-    const func = () => {
-      message.channel.send('時間です')
+    const createRespawnMessage = (name: string) => {
+      message.channel.send(`@here ${name}の沸き時間になったわ`)
     }
     if (['rasberg', 'ラスベルグ'].includes(monsterName)) {
       const id = rasberg.id
       const name = rasberg.name
       const { start, end } = createRespawnDate(20, 26)
 
-      resMessage = respawnManager(id, name, start, end, func)
+      resMessage = respawnManager(id, name, start, end, createRespawnMessage)
     } else if (['zaphiel', 'ザフィエル'].includes(monsterName)) {
       const id = zaphiel.id
       const name = zaphiel.name
       const { start, end } = createRespawnDate(36, 48)
 
-      resMessage = respawnManager(id, name, start, end, func)
+      resMessage = respawnManager(id, name, start, end, createRespawnMessage)
     } else if (['cheshti', 'チェシュチ'].includes(monsterName)) {
       const id = cheshti.id
       const name = cheshti.name
       const { start, end } = createRespawnDate(24, 32)
 
-      resMessage = respawnManager(id, name, start, end, func)
+      resMessage = respawnManager(id, name, start, end, createRespawnMessage)
     } else if (['menotios', 'メノティオス'].includes(monsterName)) {
       const id = menotios.id
       const name = menotios.name
       const { start, end } = createRespawnDate(24, 36)
 
-      resMessage = respawnManager(id, name, start, end, func)
+      resMessage = respawnManager(id, name, start, end, createRespawnMessage)
     } else if (['alukina', 'アールキナ'].includes(monsterName)) {
       const id = alukina.id
       const name = alukina.name
       const { start, end } = createRespawnDate(36, 48)
 
-      resMessage = respawnManager(id, name, start, end, func)
+      resMessage = respawnManager(id, name, start, end, createRespawnMessage)
     }
     return message.channel.send(resMessage)
   },
@@ -56,13 +56,13 @@ const respawnManager = (
   name: string,
   start: number,
   end: number,
-  func: () => void
+  func: (name: string) => void
 ) => {
   // dbに討伐時間を記録
   saveRespawnTime(id, name, start, end)
   // リマインダーの設定
   const date = new Date(start * 1000)
-  setRemainder(id, date, func)
+  setRemainder(id, date, () => func(name))
 
   // メッセージの作成
   const startStr = getFormattedDate(start)
