4b4364c2f0031bc414390ac551aab0340087fa75
nayukata <natume31612@gmail.com>
2021-08-14 11:55:30 +0900

    fix: scheduler

--- libs/schedule.ts
+++ libs/schedule.ts
@@ -1,6 +1,7 @@
 import Schedule from 'node-schedule'
 
 export const setRemainder = (id: string, date: Date, func: () => void) => {
+  date.setMinutes(date.getMinutes() - 15)
   Schedule.cancelJob(id)
   const job = Schedule.scheduleJob(id, date, func)
   return job
